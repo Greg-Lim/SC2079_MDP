8ca0423784609c74368c62a1d1fe28fa1c47e1be
YichenG170 <gaoyimingyyds@gmail.com>
2024-08-24 19:09:34 +0800

    add corresponding UIs for obstacles; add reset buttons

--- Algorithm/simulator/simulator.py
+++ Algorithm/simulator/simulator.py
@@ -11,6 +11,11 @@ CELL_SIZE = 25
 MARGIN = 50
 CONTROL_PANEL_HEIGHT = 100
 
+START_POS = [0, 0]
+START_DIRECTION = 'U'
+OBSTACLES = []
+OBSTACLE_DIRECTIONS = []
+
 # Colors
 WHITE = (255, 255, 255)
 GRAY = (200, 200, 200)
@@ -30,19 +35,29 @@ font_input_box = pygame.font.Font(None, 24)
 label_font = pygame.font.Font(None, 24)
 
 # Input fields
-input_boxes_robot_pos = {
-    'x': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 160, 65, 30), 'text': '0', 'active': False},
-    'y': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 105, MARGIN + 160, 65, 30), 'text': '0', 'active': False},
-    'direction': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 190, MARGIN + 160, 30, 30), 'text': 'U', 'active': False}
+input_boxes = {
+    'x_o': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 45, 80, 30), 'text': '0', 'active': False},
+    'y_o': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 105, MARGIN + 45, 80, 30), 'text': '0', 'active': False},
+    'direction_o': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 190, MARGIN + 45, 30, 30), 'text': 'U', 'active': False},
+
+    'x_p': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 185, 80, 30), 'text': '0', 'active': False},
+    'y_p': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 105, MARGIN + 185, 80, 30), 'text': '0', 'active': False},
+    'direction_p': {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 190, MARGIN + 185, 30, 30), 'text': 'U', 'active': False}
 }
 
 buttons = {
-    'set' : {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 200, 200, 30), 'text': 'SET', 'active': False}
+    'add' : {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 80, 125, 30), 'color': GREEN, 'text': 'ADD', 'active': False},
+    'reset_o' : {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 150, MARGIN + 80, 70, 30), 'color': RED, 'text': 'RESET', 'active': False},
+
+    'set' : {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 220, 125, 30), 'color': GREEN, 'text': 'SET', 'active': False},
+    'reset_p' : {'rect': pygame.Rect(GRID_SIZE * CELL_SIZE + MARGIN + 150, MARGIN + 220, 70, 30), 'color': RED, 'text': 'RESET', 'active': False}
 }
 
 # Global variables
-start_pos = [0, 0]
-start_direction = 'U'
+start_pos = START_POS
+start_direction = START_DIRECTION
+obstacles = OBSTACLES
+obstacle_directions = OBSTACLE_DIRECTIONS
 
 # Functions
 def draw_grid():
@@ -63,28 +78,25 @@ def draw_labels():
         screen.blit(label, (MARGIN - label.get_width() - 5, MARGIN + y * CELL_SIZE + CELL_SIZE // 2 - label.get_height() // 2))
 
 def draw_titles():
-    # Draw Add Obstacle button
     pygame.draw.rect(screen, GRAY, (GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN, 200, 40))
     text = font.render('Add Obstacle', True, BLACK)
     screen.blit(text, (GRID_SIZE * CELL_SIZE + MARGIN + 40, MARGIN + 10))
 
-    # Draw Robot Position button
-    pygame.draw.rect(screen, GRAY, (GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 100, 200, 40))
+    pygame.draw.rect(screen, GRAY, (GRID_SIZE * CELL_SIZE + MARGIN + 20, MARGIN + 140, 200, 40))
     text = font.render('Robot Position', True, BLACK)
-    screen.blit(text, (GRID_SIZE * CELL_SIZE + MARGIN + 32, MARGIN + 110))
+    screen.blit(text, (GRID_SIZE * CELL_SIZE + MARGIN + 32, MARGIN + 150))
 
 def draw_input_boxes():
-    for key, box in input_boxes_robot_pos.items():
+    for key, box in input_boxes.items():
         color = GRAY_L if box['active'] else WHITE
         pygame.draw.rect(screen, color, box['rect'])
-        pygame.draw.rect(screen, BLACK, box['rect'], 2)  # Draw border
+        pygame.draw.rect(screen, BLACK, box['rect'], 2)
         text_surface = font_input_box.render(box['text'], True, BLACK)
         screen.blit(text_surface, (box['rect'].x + 6, box['rect'].y + 8))
 
 def draw_buttons():
     for key, box in buttons.items():
-        color = GREEN
-        pygame.draw.rect(screen, color, box['rect'])
+        pygame.draw.rect(screen, box['color'], box['rect'])
         pygame.draw.rect(screen, BLACK, box['rect'], 2)
         text_surface = font_input_box.render(box['text'], True, BLACK)
         screen.blit(text_surface, (box['rect'].x + 6, box['rect'].y + 8))
@@ -109,11 +121,11 @@ def main():
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 # Handle mouse clicks to activate the correct input box
-                for key, box in input_boxes_robot_pos.items():
+                for key, box in input_boxes.items():
                     if box['rect'].collidepoint(event.pos):
                         box['active'] = True
                         
-                        if key == 'direction':
+                        if key in ['direction_o', 'direction_p']:
                             if box['text'] == 'U':
                                 box['text'] = 'D'
                             elif box['text'] == 'D':
@@ -125,7 +137,7 @@ def main():
                             
                             print(box['text'])
                             
-                            input_boxes_robot_pos['direction']['text'] = box['text']
+                            input_boxes[key]['text'] = box['text']
                     else:
                         box['active'] = False
                 
@@ -133,19 +145,45 @@ def main():
                     if box['rect'].collidepoint(event.pos):
                         box['active'] = True
 
-                        if key == 'set':
-                            start_pos[0] = int(input_boxes_robot_pos['x']['text'])
-                            start_pos[1] = int(input_boxes_robot_pos['y']['text'])
-                            start_direction = input_boxes_robot_pos['direction']['text']
+                        if key == 'add':
+                            obstacle = []
+                            obstacle.append(int(input_boxes['x_o']['text']))
+                            obstacle.append(int(input_boxes['y_o']['text']))
 
-                        print(start_pos)
+                            add_status = 1
+                            for o in obstacles:
+                                if o == obstacle:
+                                    print("Failed to add new obstacle: location occupied")
+                                    add_status = 0
+                            
+                            if add_status == 1:
+                                obstacles.append(obstacle)
+                                obstacle_directions.append(input_boxes['direction_o']['text'])
+                                print("New obstacle added to", obstacles[-1], "\nNew obstacle's direction set to", obstacle_directions[-1])
+                        
+                        elif key == 'reset_o':
+                            obstacles = OBSTACLES
+                            obstacle_directions = OBSTACLE_DIRECTIONS
+                            print("Obstacle(s) cleared")
+                        
+                        elif key == 'set':
+                            start_pos[0] = int(input_boxes['x_p']['text'])
+                            start_pos[1] = int(input_boxes['y_p']['text'])
+                            start_direction = input_boxes['direction_p']['text']
+                            print("Start position set to", start_pos, "\nStart direction set to", start_direction)
+                        
+                        elif key == 'reset_p':
+                            start_pos = START_POS
+                            start_direction = START_DIRECTION
+                            print("Start position reset to", start_pos, "\nStart direction reset to", start_direction)
+
                     else:
                         box['active'] = False
 
             elif event.type == pygame.KEYDOWN:
-                for key, box in input_boxes_robot_pos.items():
+                for key, box in input_boxes.items():
                     if box['active']:
-                        if key in ['x', 'y']:
+                        if key in ['x_o', 'y_o', 'x_p', 'y_p']:
                             if event.key == pygame.K_BACKSPACE:
                                 box['text'] = box['text'][:-1]  # Remove last character
                             elif event.unicode.isdigit():
